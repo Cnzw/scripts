66781b6e5c6cd9502ffadf12cbe15ad37bf9764b
Cnzw <git@cnzw.top>
2024-03-23 14:30:02 +0800

    Create 方正教务_教学评价.js

---
+++ 方正教务_教学评价.js
@@ -0,0 +1,14 @@
+$('table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(4) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(5) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(6) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(7) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(8) > td:nth-child(2) > div > div:nth-child(1) > label > input').click();
+$('table:nth-child(2) > tbody > tr:nth-child(9) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
+$('table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
+$('table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
+$('table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
+$('table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
+$('table:nth-child(4) > tbody > tr:nth-child(5) > td:nth-child(2) > div > div:nth-child(2) > label > input').click();
